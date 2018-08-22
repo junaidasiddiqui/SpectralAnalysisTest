0e511d7c439f07dcc18dccd0de3d962700ca7c65
Junaid Siddiqui <junaidas97@gmail.com>
2018-08-22 18:50:00 -0400

    Hanning Window funcction

--- src/MathOps.java
+++ src/MathOps.java
@@ -1,6 +1,6 @@
 public class MathOps {
 
-    public static float getMaxValue(float[] array) {
+    static float getMaxValue(float[] array) {
         float maxValue = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] > maxValue) {
@@ -11,7 +11,7 @@ public class MathOps {
     }
 
     // getting the miniumum value
-    public static float getMinValue(float[] array) {
+    static float getMinValue(float[] array) {
         float minValue = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] < minValue) {
@@ -21,11 +21,21 @@ public class MathOps {
         return minValue;
     }
 
-    public static float getMeanValue(float[] array) {
+    static float getMeanValue(float[] array) {
         float sum = 0;
         for (int i = 1; i < array.length; i++) {
             sum += array[i];
         }
         return sum / array.length;
+    }
+
+    public short[] HanningWindow(short[] signal_in, int pos, int size)
+    {
+        for (int i = pos; i < pos + size; i++)
+        {
+            int j = i - pos; // j = index into Hann window function
+            signal_in[i] = (short) (signal_in[i] * 0.5 * (1.0 - Math.cos(2.0 * Math.PI * j / size)));
+        }
+        return signal_in;
     }
 }
